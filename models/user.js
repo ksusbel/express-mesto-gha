44a04c78b60d93f89f254f6f834908a14a7d444a
Oksana Belyakova <ksusbel@yandex.ru>
2023-11-18 16:16:17 +0300

    18.11-2

--- models/user.js
+++ models/user.js
@@ -4,25 +4,20 @@ const userScheme = new mongoose.Schema(
     {
         name: {
             type: String,
-            minlength: [2, "Минимальная длинна 2 символа"],
-            maxlength: [30, "Максимальная длинна 30 символа"],
-            default: "Жак-Ив Кусто",
+            minlength: 2,
+            maxlength: 30,
             required: true
         },
         about: {
             type: String,
-            minlength: [2, "Минимальная длинна 2 символа"],
-            maxlength: [30, "Максимальная длинна 30 символа"],
-            default: "Исследователь",
+            minlength: 2,
+            maxlength: 30,
             required: true
         },
         avatar: {
             type: String,
             default: "",
-            pattern: {
-                params: /^https?:\/\/(?:[\w-]+\.)+[a-z]{2,}(?:\/\S*)?$/i,
-                message: "Должен начинаться с http, https, проверьте правильность формата",
-            },
+            pattern: /^https?:\/\/(?:[\w-]+\.)+[a-z]{2,}(?:\/\S*)?$/i,
             required: true
         },
     },
