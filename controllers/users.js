4cab36dc23e8c5eb627c9a8655da9b5a1cacb955
Oksana Belyakova <ksusbel@yandex.ru>
2023-11-18 16:39:23 +0300

    18.11-5

--- controllers/users.js
+++ controllers/users.js
@@ -15,6 +15,10 @@ module.exports.getUserById = async (req, res) => {
     try {
         const { userId } = req.params;
         const user = await User.findById(userId);
+
+        if (!user) {
+          return res.status(404).send({ message: "Пользователь не найден" });
+        }
         res.status(200).send(user);
     } catch (error) {
         console.log(error);
