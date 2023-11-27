f6a0ca2f783cee3b6d31c193f0f0dcd9dcd0fa53
Oksana Belyakova <ksusbel@yandex.ru>
2023-11-27 15:37:26 +0300

    27.11

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,33 @@
 const jwt = require('jsonwebtoken');
 
+
+const { JWT_SECRET, NODE_ENV } = process.env;
 module.exports = (req, res, next) => {
-
- //  console.log(req.headers.cookie);
-    const authorization = req.headers;
-//  console.log(authorization);
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    // const token = req.headers.authorization;
+    const token = req.cookies.parrotToken;
+
+    if (!token) {
+      throw new Error("NotAutanticate");
+    }
+
+    const validTocken = token.replace("Bearer ", "");
+    payload = jwt.verify(validTocken, NODE_ENV ? JWT_SECRET : "dev_secret");
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    if (err.message === "NotAutanticate") {
+      return res
+        .status(401)
+        .send({ message: "Не правильные email или password" });
+    }
+
+    if ((err.name = "JsonWebTokenError")) {
+      return res.status(401).send({ message: "С токеном что-то не так" });
+    }
+
+    return res.status(500).send(err);
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
-
-  next(); // пропускаем запрос дальше
-};
+  req.user = payload;
+  next();
+}
